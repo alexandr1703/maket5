d21a8ed945d1a528ba63844c8edc1fe5735f66c1
Alexandr 1703 <aleks628202@gmail.com>
2021-03-31 17:58:15 +0300

    perf

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,9 +1,9 @@
 $('.slider__com').slick({
     infinite: true,
     dots: true,
-    autoplay: true,
-    autoplaySpeed:10000,
-    fade:true,
-    slidesToShow: 1,
+    arrows:false,
+    // autoplay: true,
+    // autoplaySpeed:10000,
+    slidesToShow: 2,
     slidesToScroll: 1
 });
